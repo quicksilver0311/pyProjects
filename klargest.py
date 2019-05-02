46452fdf803b3888633155e4c88019d9ee13e561
Jay Shah <jay.shah@intel.com>
2019-05-01 21:43:23 -0700

    Initial commit

---
+++ klargest.py
@@ -0,0 +1,9 @@
+def klargest(k, arr):
+	arr.sort()
+	arr=arr[::-1]
+	for i in range(k):
+		print(arr[i])
+	
+input=[1,2,6,9,2134,65356,234324,12,34]
+k=2
+klargest(k,input)
---
+++ pythagorean_triplet.py
@@ -0,0 +1,24 @@
+'''Time complexity: O(n*n)'''
+
+def containstriplet(nums):
+	for i in range(len(nums)):
+		nums[i]=nums[i]*nums[i];
+	nums.sort()
+
+	for i in range(len(nums)-1, 1, -1):
+		j=0
+		k=i-1
+		while(j<k):
+			if(nums[j]+nums[k]==nums[i]):
+				return True
+			elif(nums[i]>nums[j]+nums[k]):
+				j+=1
+			else:
+				k-=1
+	return False
+
+input=[1,2,3,4,5,6,7,8,9]
+if(containstriplet(input)):
+	print("Yes")
+else:
+	print("No")
